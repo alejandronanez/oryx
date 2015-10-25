83a6719719656c0dc3c5b610dd5e052ba42ed47c
Alejandro Nanez <alejonanez@gmail.com>
2015-10-25 17:09:46 -0500

    Moving axis

--- 103.js
+++ 103.js
@@ -1,5 +1,9 @@
+'use strict';
+
 var dualShock = require('dualshock-controller');
 var RollingSpider = require('rolling-spider');
+var vertical = 50;
+var horizontal = 50;
 
 var ps4Controller = dualShock({
         config : "dualshock4-generic-driver",
@@ -17,39 +21,91 @@ function printBattery() {
     console.log(`Battery: ${rollingSpider.status.battery}%`);
 }
 
-rollingSpider.connect(function () {
-    rollingSpider.setup(function () {
-        console.log(`Configured for Rolling Spider! ${rollingSpider.name}`);
-        rollingSpider.flatTrim();
-        rollingSpider.startPing();
-        rollingSpider.flatTrim();
+/**
+ * Used to change horizontal/vertical values according to user input on the controller
+ * @param  {String} direction What key did the user press
+ * @return {Number}           The new value for vertical or horizontal
+ */
+function manipulateAxis(direction) {
+    let options = {
+        dpadUp() {
+            if (vertical <= 95) {
+                vertical = vertical + 5;
+            }
+            return vertical;
+        },
+        dpadRight() {
+            if (horizontal <= 95) {
+                horizontal = horizontal + 5
+            }
 
-         rollingSpider.on('battery', printBattery);
+            return horizontal;
+        },
+        dpadDown() {
+            if (vertical >= 5) {
+                vertical = vertical - 5
+            }
 
-        setTimeout(function () {
-            console.log('Ready for Flight');
-        }, 500);
-    });
-});
+            return vertical;
+        },
+        dpadLeft() {
+            if (horizontal >= 5) {
+                horizontal = horizontal - 5
+            }
+            return horizontal;
+        },
+    }
+
+    if (options.hasOwnProperty(direction)) {
+        return options[direction]();
+    }
+}
+
+// rollingSpider.connect(function () {
+//     rollingSpider.setup(function () {
+//         console.log(`Configured for Rolling Spider! ${rollingSpider.name}`);
+//         rollingSpider.flatTrim();
+//         rollingSpider.startPing();
+//         rollingSpider.flatTrim();
+
+//          rollingSpider.on('battery', printBattery);
+
+//         setTimeout(function () {
+//             console.log('Ready for Flight');
+//         }, 500);
+//     });
+// });
 
 /**
  * Add listeners for the controller
  */
-ps4Controller.on('dpadDown:press', (thing) => {
+ps4Controller.on('dpadDown:press', (direction) => {
+    console.log(manipulateAxis(direction))
     console.log('DOWN');
 });
 
-ps4Controller.on('dpadUp:press', (thing) => {
+ps4Controller.on('dpadUp:press', (direction) => {
+    console.log(manipulateAxis(direction))
     console.log('UUUP');
 });
 
-ps4Controller.on('share:press', (thing) => {
+ps4Controller.on('dpadRight:press', (direction) => {
+    console.log(manipulateAxis(direction))
+    console.log('Right');
+});
+
+ps4Controller.on('dpadLeft:press', (direction) => {
+    console.log(manipulateAxis(direction))
+    console.log('Left');
+});
+
+ps4Controller.on('share:press', () => {
     console.log('OFF');
     rollingSpider.land();
     process.exit(0);
 });
 
-ps4Controller.on('options:press', (thing) => {
+ps4Controller.on('options:press', () => {
     console.log('ON');
     rollingSpider.takeOff();
     rollingSpider.flatTrim();
@@ -58,22 +114,22 @@ ps4Controller.on('options:press', (thing) => {
 /**
  * Rolling!
  */
-ps4Controller.on('triangle:press', (thing) => {
+ps4Controller.on('triangle:press', () => {
     console.log('FRONTFLIP');
     rollingSpider.frontFlip();
 });
 
-ps4Controller.on('circle:press', (thing) => {
+ps4Controller.on('circle:press', () => {
     console.log('RIGHT FLIP');
     rollingSpider.rightFlip();
 });
 
-ps4Controller.on('x:press', (thing) => {
+ps4Controller.on('x:press', () => {
     console.log('BACKFLIP');
     rollingSpider.backFlip();
 });
 
-ps4Controller.on('square:press', (thing) => {
+ps4Controller.on('square:press', () => {
     console.log('LEFTFLIP');
     rollingSpider.leftFlip();
 });
